8c819520aae4b9a641fb62baf01d804a9d1b5279
altiz <you@example.com>
2020-10-15 13:59:14 -0700

    testing Ok

--- cmd/webserver/main_test.go
+++ cmd/webserver/main_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -15,6 +14,9 @@ import (
 type data struct {
 	User string `json:"user"`
 }
+type data1 struct {
+	Status int
+}
 
 func TestMain(t *testing.T) {
 	router := setupRouter()
@@ -22,7 +24,7 @@ func TestMain(t *testing.T) {
 	Convey("Users endpoints should respond correctly", t, func() {
 		Convey("User should return empty list", func() {
 			// it's safe to ignore error here, because we're manually entering URL
-			req, _ := http.NewRequest("GET", "http://localhost:8080/v1/", nil)
+			req, _ := http.NewRequest("GET", "http://localhost:8080/api/v1/", nil)
 			w := httptest.NewRecorder()
 			router.ServeHTTP(w, req)
 
@@ -32,21 +34,24 @@ func TestMain(t *testing.T) {
 		})
 
 		Convey("Create should return ID of newly created user", func() {
-			user := &data{User: "Test user"}
+			//send message
+			user := &data{User: "test"}
 			dat, err := json.Marshal(user)
 			So(err, ShouldBeNil)
 			buf := bytes.NewBuffer(dat)
-			req, err := http.NewRequest("POST", "http://localhost/v1/test", buf)
+			req, err := http.NewRequest("POST", "http://localhost/api/v1/test", buf)
 			So(err, ShouldBeNil)
 			w := httptest.NewRecorder()
 			router.ServeHTTP(w, req)
 			So(w.Code, ShouldEqual, http.StatusOK)
-
+			//resp
 			body := w.Body.Bytes()
-			var users []*data
-			json.Unmarshal(body, &user)
-			fmt.Println(user)
-			So(len(users), ShouldEqual, 1)
+			var obj *data1
+
+			if err := json.Unmarshal(body, &obj); err != nil {
+				panic(err)
+			}
+			So(obj.Status, ShouldEqual, 0)
 		})
 	})
 }
--- cmd/webserver/routes/routes.go
+++ cmd/webserver/routes/routes.go
@@ -22,17 +22,27 @@ func testJSON(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	logs.WithFields(logs.Fields{
-		"User": json.User,
-	}).Info("Ok")
-	c.JSON(http.StatusOK, gin.H{
-		"User": json.User,
-	})
+	if json.User == "test" {
+		logs.WithFields(logs.Fields{
+			"Status": 0,
+		}).Info("Ok")
+		c.JSON(http.StatusOK, gin.H{
+			"Status": 0,
+		})
+	} else {
+		logs.WithFields(logs.Fields{
+			"User": json.User,
+		}).Info("Ok")
+		c.JSON(http.StatusOK, gin.H{
+			"User": json.User,
+		})
+	}
 
 }
 
 func InitializeRoutes(router *gin.Engine) {
-	version1 := router.Group("/v1")
+	api := router.Group("/api")
+	version1 := api.Group("/v1")
 
 	version1.POST("/test", testJSON)
 	version1.GET("/", indexPage)
